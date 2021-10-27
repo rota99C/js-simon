dd2aa83c93e6717962c4e41069dde24ea8c9807a
rota99C <rotaboolean@gmail.com>
2021-10-27 16:06:57 +0200

    creato numeri random il ciclo while + messaggio alert come richiesto dal esercizio

---
+++ main.js
@@ -0,0 +1,43 @@
+/* Un alert() espone 5 numeri generati casualmente.
+Da li parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+
+//strumenti
+//allert
+//mathrandom
+//ciclo while
+
+
+
+//numeri random
+//creo un array dove salvo i numeri random
+
+let numeri = []
+while (numeri.length <= 5) {
+    let random = Math.floor(math.random() * 10)
+    if (numeri.includes(random)) {
+        numeri.push(random)
+    }
+}
+
+
+//messaggio allert
+
+let messaggio = alert("ciao , memorizza bene questi numeri che ho gnerato + 'numeri'  Poi ti dirò quanti ne hai indovinati")
+
+//una variabile dove inserisco i nuemri inseriti
+let backupNumeri = []
+
+setTimeout(function () {
+    for (let index = 0; index < 5; i++) {
+        //domanda all'utente
+        let risposta = parseInt(prompt("dimmi che numeri ricordi"));
+        if (numeri.includes(risposta)) {
+            backupNumeri.push(risposta);
+        }
+    }
+
+    let finale = alert("hai indovinato + 'risposta.lenght ")
+})
